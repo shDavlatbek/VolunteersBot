773c73fa791d0e0ac2d65dbaa5ed8b993f3c6a36
GFmatrix <sketchapps.lp@gmail.com>
2024-08-23 20:05:38 +0500

    Admin update

--- app/admin.py
+++ app/admin.py
@@ -15,5 +15,25 @@ class MessageAdmin(admin.ModelAdmin):
     class Media:
         js = ('app/custom_filter.js',)  # Link to the custom JavaScript file
 
+class UserAdmin(admin.ModelAdmin):
+    # Specify the fields you want to be searchable
+    search_fields = ['telegram_id', 'first_name', 'last_name']
+
+    # Optionally, you can also customize the list display and other options
+    list_display = ['telegram_id', 'first_name', 'last_name']
+    list_filter = ['guests']
+
+
+class GuestAdmin(admin.ModelAdmin):
+    # Specify which fields should be searchable
+    search_fields = ['full_name', 'name_of_group']
+    
+    # Specify which fields should be used for filtering in the admin interface
+    list_filter = ['state', 'language', 'sex']
+    
+    # Optionally, you can customize the list display
+    list_display = ['full_name', 'name_of_group', 'state', 'sex']
+
 admin.site.register(Message, MessageAdmin)
-admin.site.register([User, Guest, Language])
+admin.site.register(User, UserAdmin)
+admin.site.register(Guest, GuestAdmin)
--- static/app/custom_filter.js
+++ static/app/custom_filter.js
@@ -2,31 +2,77 @@ document.addEventListener('DOMContentLoaded', function() {
     const categorySelect = document.querySelector('#id_categories');
     const userSelect = document.querySelector('#id_user');
 
-    categorySelect.addEventListener('change', function() {
-        const selectedCategories = Array.from(categorySelect.selectedOptions).map(option => option.value);
-        console.log('Selected categories:', selectedCategories);
-        console.log('Selected categories:', selectedCategories.join(','));
-        
-        if (selectedCategories.length > 0) {
-            // Make an AJAX call to fetch users based on selected categories
-            fetch(`/helper/get_users_by_category/?categories=${selectedCategories.join(',')}`)
-                .then(response => response.json())
-                .then(data => {
-                    // Clear the existing options
-                    userSelect.innerHTML = '';
-                    console.log(data);
-                    
-                    // Populate the user select field with new options
-                    data.users.forEach(user => {
-                        const option = document.createElement('option');
-                        option.value = user.id;
-                        option.text = user.name;
-                        userSelect.add(option);
-                    });
 
-                    // Reinitialize the Select2 widget
-                    $(userSelect).trigger('change');
-                });
-        }
+    function waitForElement(selector, callback) {
+        var interval = setInterval(function() {
+            var element = document.querySelector(selector);
+            if (element) {
+                clearInterval(interval);
+                callback(element);
+            }
+        }, 100); // Check every 100 milliseconds
+    }
+    
+    
+    waitForElement('#id_categories_add_link', function() {
+        
+        const categorySelectFrom = document.querySelector('#id_categories_from');
+        const categorySelectTo = document.querySelector('#id_categories_to');
+        const categoryAddLink = document.querySelector('#id_categories_add_link');
+        const categoryRemoveLink = document.querySelector('#id_categories_remove_link');
+        const userSelect = document.querySelector('#id_user');
+    
+        categoryAddLink.addEventListener('click', function() {
+            const selectedCategories = Array.from(categorySelect.selectedOptions).map(option => option.value);
+            console.log(selectedCategories);
+            
+            if (selectedCategories.length > 0) {
+                // Make an AJAX call to fetch users based on selected categories
+                fetch(`/helper/get_users_by_category/?categories=${selectedCategories.join(',')}`)
+                    .then(response => response.json())
+                    .then(data => {
+                        // Clear the existing options
+                        userSelect.innerHTML = '';
+                        console.log(data);
+                        
+                        // Populate the user select field with new options
+                        data.users.forEach(user => {
+                            const option = document.createElement('option');
+                            option.value = user.id;
+                            option.text = user.name;
+                            userSelect.add(option);
+                        });
+    
+                        // Reinitialize the Select2 widget
+                        $(userSelect).trigger('change');
+                    });
+            }
+        });
     });
+    
+
+    // categorySelect.addEventListener('change', function() {
+    //     const selectedCategories = Array.from(categorySelect.selectedOptions).map(option => option.value);
+        
+    //     if (selectedCategories.length > 0) {
+    //         // Make an AJAX call to fetch users based on selected categories
+    //         fetch(`/helper/get_users_by_category/?categories=${selectedCategories.join(',')}`)
+    //             .then(response => response.json())
+    //             .then(data => {
+    //                 // Clear the existing options
+    //                 userSelect.innerHTML = '';
+                    
+    //                 // Populate the user select field with new options
+    //                 data.users.forEach(user => {
+    //                     const option = document.createElement('option');
+    //                     option.value = user.id;
+    //                     option.text = user.name;
+    //                     userSelect.add(option);
+    //                 });
+
+    //                 // Reinitialize the Select2 widget
+    //                 $(userSelect).trigger('change');
+    //             });
+    //     }
+    // });
 });
